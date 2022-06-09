ff4e9da6506d1df5a616f51e662c50f327d3e1a8
Francesco Ceccon <francesco@ceccon.me>
2022-06-09 22:13:02 +0100

    feat: update chain head

--- apibara/src/head_tracker/mod.rs
+++ apibara/src/head_tracker/mod.rs
@@ -14,9 +14,9 @@ const CHANNEL_BUFFER_SIZE: usize = 64;
 /// Message sent by the head tracker.
 #[derive(Debug, Clone)]
 pub enum Message {
-    /// New block produced.
+    /// New block produced. This is now the head.
     NewBlock(BlockHeader),
-    /// Chain reorganization.
+    /// Chain reorganization. The specified block is the new head.
     Reorg(BlockHeader),
 }
 
@@ -42,6 +42,47 @@ impl<P: ChainProvider> HeadTracker<P> {
         Ok((join_handle, stream))
     }
 
+    async fn run_loop(self, _tx: mpsc::Sender<Message>) -> Result<()> {
+        let mut block_buffer = self.build_chain_reorg_buffer().await?;
+        let mut block_stream = self.provider.subscribe_blocks()?;
+        while let Some(block) = block_stream.next().await {
+            debug!(
+                "received new block {} {} (p: {:?})",
+                block.number, block.hash, block.parent_hash
+            );
+            let prev_head = match block_buffer.front() {
+                None => return Err(Error::msg("head tracker buffer is empty")),
+                Some(prev_head) if prev_head.hash == block.hash => {
+                    // stream sent the same block twice
+                    continue;
+                }
+                Some(prev_head) => prev_head.clone(),
+            };
+
+            debug!("prev head: {} {}", prev_head.number, prev_head.hash);
+            if block
+                .parent_hash
+                .clone()
+                .map(|h| h == prev_head.hash)
+                .unwrap_or(false)
+            {
+                debug!("clean head application");
+                // push to the _front_ of the queue
+                block_buffer.push_front(block);
+
+                // only empty buffer every now and then
+                while block_buffer.len() > 2 * self.reorg_buffer_size {
+                    block_buffer.pop_back();
+                }
+            } else if block.number > prev_head.number + 1 {
+                panic!("head application with gaps not handled");
+            } else {
+                panic!("head application rollback not handled");
+            }
+        }
+        Ok(())
+    }
+
     async fn build_chain_reorg_buffer(&self) -> Result<VecDeque<BlockHeader>> {
         let current_head = self.provider.get_head_block().await?;
 
@@ -64,20 +105,14 @@ impl<P: ChainProvider> HeadTracker<P> {
                             Error::msg(format!("expected block with hash {} to exist", hash))
                         })?;
                     next_hash = block.parent_hash.clone();
+
+                    // push to the _back_ of the queue because the loop is walking the
+                    // chain backwards.
                     block_buffer.push_back(block);
                 }
             }
         }
 
         Ok(block_buffer)
-    }
-
-    async fn run_loop(self, _tx: mpsc::Sender<Message>) -> Result<()> {
-        let mut _block_buffer = self.build_chain_reorg_buffer().await?;
-        let mut block_stream = self.provider.subscribe_blocks()?;
-        while let Some(_block) = block_stream.next().await {
-            debug!("received block");
-        }
-        Ok(())
     }
 }
