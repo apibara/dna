4f49477feb44d658a4fdb3714bcd29699620a927
Valentin Dosimont <valentin@pupucecorp.com>
2023-02-28 11:39:30 +0100

    feat: init sdk workspace + high level implementation api

---
+++ sdk/src/lib.rs
@@ -0,0 +1,354 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use futures::Stream;
+use pb::stream::v1alpha2::{stream_client::StreamClient, StreamDataResponse};
+use pin_project::pin_project;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
+use tokio_util::sync::CancellationToken;
+use tonic::{transport::Channel, Streaming};
+
+use crate::pb::{
+    starknet::v1alpha2::Filter,
+    stream::v1alpha2::{Cursor, DataFinality, StreamDataRequest},
+};
+
+pub struct Raw;
+pub struct Configured;
+
+pub mod pb {
+    pub mod starknet {
+        pub mod v1alpha2 {
+            use std::fmt::Display;
+
+            use prost::Message;
+            tonic::include_proto!("apibara.starknet.v1alpha2");
+
+            #[derive(Clone, PartialEq, Message)]
+            pub struct BlockBody {
+                #[prost(message, repeated, tag = "1")]
+                pub transactions: prost::alloc::vec::Vec<Transaction>,
+            }
+
+            #[derive(Clone, PartialEq, Message)]
+            pub struct BlockReceipts {
+                #[prost(message, repeated, tag = "1")]
+                pub receipts: prost::alloc::vec::Vec<TransactionReceipt>,
+            }
+
+            pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+                tonic::include_file_descriptor_set!("starknet_descriptor_v1alpha2");
+
+            pub fn starknet_file_descriptor_set() -> &'static [u8] {
+                FILE_DESCRIPTOR_SET
+            }
+
+            impl BlockStatus {
+                pub fn is_finalized(&self) -> bool {
+                    *self == BlockStatus::AcceptedOnL1
+                }
+
+                pub fn is_accepted(&self) -> bool {
+                    *self == BlockStatus::AcceptedOnL2
+                }
+
+                pub fn is_rejected(&self) -> bool {
+                    *self == BlockStatus::Rejected
+                }
+            }
+
+            impl FieldElement {
+                pub fn from_u64(value: u64) -> FieldElement {
+                    FieldElement {
+                        lo_lo: 0,
+                        lo_hi: 0,
+                        hi_lo: 0,
+                        hi_hi: value,
+                    }
+                }
+
+                pub fn from_bytes(bytes: &[u8; 32]) -> Self {
+                    let lo_lo = u64::from_be_bytes([
+                        bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6],
+                        bytes[7],
+                    ]);
+                    let lo_hi = u64::from_be_bytes([
+                        bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14],
+                        bytes[15],
+                    ]);
+                    let hi_lo = u64::from_be_bytes([
+                        bytes[16], bytes[17], bytes[18], bytes[19], bytes[20], bytes[21],
+                        bytes[22], bytes[23],
+                    ]);
+                    let hi_hi = u64::from_be_bytes([
+                        bytes[24], bytes[25], bytes[26], bytes[27], bytes[28], bytes[29],
+                        bytes[30], bytes[31],
+                    ]);
+
+                    FieldElement {
+                        lo_lo,
+                        lo_hi,
+                        hi_lo,
+                        hi_hi,
+                    }
+                }
+
+                pub fn to_bytes(&self) -> [u8; 32] {
+                    let lo_lo = self.lo_lo.to_be_bytes();
+                    let lo_hi = self.lo_hi.to_be_bytes();
+                    let hi_lo = self.hi_lo.to_be_bytes();
+                    let hi_hi = self.hi_hi.to_be_bytes();
+                    [
+                        lo_lo[0], lo_lo[1], lo_lo[2], lo_lo[3], lo_lo[4], lo_lo[5], lo_lo[6],
+                        lo_lo[7], lo_hi[0], lo_hi[1], lo_hi[2], lo_hi[3], lo_hi[4], lo_hi[5],
+                        lo_hi[6], lo_hi[7], hi_lo[0], hi_lo[1], hi_lo[2], hi_lo[3], hi_lo[4],
+                        hi_lo[5], hi_lo[6], hi_lo[7], hi_hi[0], hi_hi[1], hi_hi[2], hi_hi[3],
+                        hi_hi[4], hi_hi[5], hi_hi[6], hi_hi[7],
+                    ]
+                }
+            }
+
+            impl Display for FieldElement {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    write!(f, "0x{}", hex::encode(self.to_bytes()))
+                }
+            }
+        }
+    }
+
+    pub mod stream {
+        pub mod v1alpha2 {
+            tonic::include_proto!("apibara.node.v1alpha2");
+
+            pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+                tonic::include_file_descriptor_set!("starknet_descriptor_v1alpha2");
+
+            pub fn stream_file_descriptor_set() -> &'static [u8] {
+                FILE_DESCRIPTOR_SET
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Configuration {
+    batch_size: u64,
+    starting_cursor: Option<Cursor>,
+    finality: Option<i32>,
+    filter: Option<Filter>,
+}
+impl Configuration {
+    pub fn new(
+        batch_size: u64,
+        starting_cursor: Option<Cursor>,
+        finality: Option<i32>,
+        filter: Option<Filter>,
+    ) -> Self {
+        Self {
+            batch_size,
+            starting_cursor,
+            finality,
+            filter,
+        }
+    }
+}
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            batch_size: 1,
+            starting_cursor: None,
+            finality: None,
+            filter: Some(Filter::default()),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ApibaraIndexerError {
+    #[error("Failed to build indexer")]
+    FailedToBuildIndexer,
+    #[error(transparent)]
+    TonicError(#[from] tonic::transport::Error),
+}
+
+pub struct ApibaraIndexer<StreamClient> {
+    client: Option<StreamClient>,
+    configuration: Option<Configuration>,
+    configuration_channel: Option<(Sender<Configuration>, Receiver<Configuration>)>,
+    inner_channel: Option<(Sender<StreamDataRequest>, Receiver<StreamDataRequest>)>,
+    ct: Option<CancellationToken>,
+}
+
+impl<StreamClient> Default for ApibaraIndexer<StreamClient> {
+    fn default() -> Self {
+        Self {
+            client: None,
+            configuration: None,
+            configuration_channel: None,
+            inner_channel: None,
+            ct: None,
+        }
+    }
+}
+
+pub struct StreamHandler {
+    stream_id: u64,
+    configuration_rx: Receiver<Configuration>,
+    ct: CancellationToken,
+    inner: StreamingClientWrapper,
+    inner_tx: Sender<StreamDataRequest>,
+}
+
+#[derive(Debug)]
+#[pin_project]
+struct StreamingClientWrapper {
+    #[pin]
+    inner: Streaming<StreamDataResponse>,
+}
+
+impl ApibaraIndexer<StreamClient<Channel>> {
+    pub fn with_client(mut self, client: StreamClient<Channel>) -> Self {
+        self.client = Some(client);
+        self
+    }
+
+    pub fn with_configuration(mut self, configuration: Configuration) -> Self {
+        self.configuration = Some(configuration);
+        self
+    }
+    pub fn with_configuration_channel(
+        mut self,
+        configuration_channel: (Sender<Configuration>, Receiver<Configuration>),
+    ) -> Self {
+        self.configuration_channel = Some(configuration_channel);
+        self
+    }
+
+    pub fn with_inner_channel(
+        mut self,
+        inner_channel: (Sender<StreamDataRequest>, Receiver<StreamDataRequest>),
+    ) -> Self {
+        self.inner_channel = Some(inner_channel);
+        self
+    }
+
+    pub fn with_cancellation_token(mut self, ct: CancellationToken) -> Self {
+        self.ct = Some(ct);
+        self
+    }
+
+    /// Apibara indexer SDK
+    ///
+    /// let base_configuration = Configuration::default();
+    /// let (mut stream, _configuration_handle) =
+    ///     ApibaraIndexer::default().build().await?;
+    /// while let Some(response) = stream.next().await {
+    ///     // Handle your own business logic in there.
+    /// }
+    pub async fn build(
+        self,
+        indexer_url: Option<String>,
+    ) -> Result<(StreamHandler, Sender<Configuration>), ApibaraIndexerError> {
+        let mut client = self.client.unwrap_or(
+            StreamClient::connect(indexer_url.unwrap_or("http://0.0.0.0:7171".into())).await?,
+        );
+        let configuration = self.configuration.unwrap_or(Configuration::default());
+        let (configuration_tx, configuration_rx) = self
+            .configuration_channel
+            .unwrap_or(tokio::sync::mpsc::channel(1));
+        let (inner_tx, inner_rx) = self.inner_channel.unwrap_or(tokio::sync::mpsc::channel(1));
+        let ct = self.ct.unwrap_or(CancellationToken::new());
+
+        configuration_tx
+            .send(configuration.clone())
+            .await
+            .expect("failed to configure initial stream");
+        Ok((
+            StreamHandler {
+                stream_id: 0,
+                configuration_rx,
+                ct,
+                inner: StreamingClientWrapper {
+                    inner: client
+                        .stream_data(ReceiverStream::new(inner_rx))
+                        .await
+                        .expect("failed to start streaming apibara")
+                        .into_inner(),
+                },
+                inner_tx,
+            },
+            configuration_tx,
+        ))
+    }
+}
+
+impl Stream for StreamHandler {
+    type Item = Result<StreamDataResponse, Box<dyn std::error::Error>>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        // directly stop stream if cancel is catched
+        if self.ct.is_cancelled() {
+            return Poll::Ready(None);
+        }
+
+        match self.configuration_rx.poll_recv(cx) {
+            Poll::Ready(None) => Poll::Ready(None),
+            Poll::Ready(Some(configuration)) => {
+                self.stream_id += 1;
+                let request = StreamDataRequest {
+                    stream_id: Some(self.stream_id),
+                    batch_size: Some(configuration.batch_size),
+                    starting_cursor: configuration.starting_cursor,
+                    finality: configuration.finality,
+                    filter: configuration.filter,
+                };
+
+                self.inner_tx
+                    .try_send(request)
+                    .expect("failed to send request");
+                cx.waker().wake_by_ref();
+                return Poll::Pending;
+            }
+            Poll::Pending => match Pin::new(&mut self.inner.inner).poll_next(cx) {
+                Poll::Ready(Some(Ok(response))) => {
+                    if response.stream_id != self.stream_id {
+                        cx.waker().wake_by_ref();
+                        return Poll::Pending;
+                    }
+
+                    return Poll::Ready(Some(Ok(response)));
+                }
+                Poll::Ready(None) => Poll::Ready(None),
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(Box::new(e)))),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ApibaraIndexer, Configuration};
+    use futures_util::StreamExt;
+
+    #[tokio::test]
+    async fn test_it_can_build() -> Result<(), Box<dyn std::error::Error>> {
+        println!("THIS IS THE TESTING OF THE NIGHT");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_apibara_high_level_api() -> Result<(), Box<dyn std::error::Error>> {
+        let (mut stream, _configuration_handle) = ApibaraIndexer::default()
+            .build(Some("http://0.0.0.0:7171".into()))
+            .await?;
+        while let Some(response) = stream.next().await {
+            println!("Response: {:?}", response);
+        }
+
+        Ok(())
+    }
+}
