740234b55f6fea99ecfab2dcb994d9be3e94470c
Francesco Ceccon <francesco@ceccon.me>
2023-03-10 10:47:17 +0000

    infra: build binaries with flake

--- flake.nix
+++ flake.nix
@@ -4,33 +4,79 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, cargo2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
+        # setup overlay with stable rust.
         overlays = [
           (import rust-overlay)
+          (import ./nix/overlay.nix)
+          cargo2nix.overlays.default
         ];
+
+        # update packages to use the current system and overlay.
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          packageFun = import ./Cargo.nix;
+          rustToolchain = pkgs.rustVersion;
+          packageOverrides = pkgs: pkgs.apibaraBuilder.overrides;
+        };
+
+        dockerizeCrateBin = { crate, volumes ? null, ports ? null }:
+          pkgs.dockerTools.buildImage {
+            name = crate.name;
+            copyToRoot = with pkgs.dockerTools; [
+              usrBinEnv
+              binSh
+              caCertificates
+            ];
+            config = {
+              Cmd = [
+                "${crate.bin}/bin/${crate.name}"
+              ];
+              Volumes = volumes;
+              Ports = ports;
+            };
+          };
       in
-        {
-          devShells.default = pkgs.mkShell {
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-            buildInputs = with pkgs; [
+      {
+        # format with `nix fmt`
+        formatter = pkgs.nixpkgs-fmt;
+
+        # development shells. start with `nix develop`.
+        devShells = {
+          default = rustPkgs.workspaceShell {
+            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
+            nativeBuildInputs = with pkgs; [
               clang
-              openssl
               pkg-config
+              llvmPackages.libclang
               protobuf
-              rust-analyzer
-              (rust-bin.stable.latest.default.override {
-                extensions = [ "rust-src" ];
-              })
             ];
           };
-        }
+        };
+
+        packages = {
+          # libs
+          core-lib = rustPkgs.workspace.apibara-core { };
+          node-lib = rustPkgs.workspace.apibara-node { };
+          sdk-lib = rustPkgs.workspace.apibara-sdk { };
+
+          # docker images
+          starknet-image = dockerizeCrateBin {
+            crate = (rustPkgs.workspace.apibara-starknet { });
+            ports = {
+              "7171/tcp" = { };
+            };
+          };
+        };
+      }
     );
 }
