f86aef72f87f2249ab89986f0b0a3604cf4860fe
Francesco Ceccon <francesco@ceccon.me>
2023-04-27 09:28:46 +0100

    webhook: call user-provided sink for each message

--- sink-common/src/connector.rs
+++ sink-common/src/connector.rs
@@ -1,17 +1,28 @@
 use std::marker::PhantomData;
 
+use apibara_core::node::v1alpha2::{Cursor, DataFinality};
 use apibara_sdk::{ClientBuilder, Configuration, DataMessage, Uri};
 use async_trait::async_trait;
 use prost::Message;
 use tokio_stream::StreamExt;
 use tokio_util::sync::CancellationToken;
+use tracing::{debug, warn};
 
 #[async_trait]
-pub trait Sink {
+pub trait Sink<B>
+where
+    B: Message,
+{
     type Error: std::error::Error + Send + Sync + 'static;
 
-    async fn handle_data(&mut self) -> Result<(), Self::Error>;
-    async fn handle_invalidate(&mut self) -> Result<(), Self::Error>;
+    async fn handle_data(
+        &mut self,
+        cursor: Option<Cursor>,
+        end_cursor: Cursor,
+        finality: DataFinality,
+        batch: Vec<B>,
+    ) -> Result<(), Self::Error>;
+    async fn handle_invalidate(&mut self, cursor: Option<Cursor>) -> Result<(), Self::Error>;
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -20,6 +31,8 @@ pub enum SinkConnectorError {
     ClientBuilder(#[from] apibara_sdk::ClientBuilderError),
     #[error("Failed to send configuration")]
     SendConfiguration,
+    #[error("Stream error: {0}")]
+    Stream(#[from] Box<dyn std::error::Error>),
 }
 
 pub struct SinkConnector<F, B>
@@ -53,37 +66,68 @@ where
         ct: CancellationToken,
     ) -> Result<(), SinkConnectorError>
     where
-        S: Sink,
+        S: Sink<B>,
     {
+        debug!("start consume stream");
         let (mut data_stream, data_client) = ClientBuilder::<F, B>::default()
-            .connect(self.stream_url)
+            .connect(self.stream_url.clone())
             .await?;
 
-        println!("send configuration = {:?}", self.configuration);
+        debug!(configuration = ?self.configuration, "sending configuration");
         data_client
-            .send(self.configuration)
+            .send(self.configuration.clone())
             .await
             .map_err(|_| SinkConnectorError::SendConfiguration)?;
 
-        while let Some(data) = data_stream.try_next().await.unwrap() {
-            match data {
-                DataMessage::Data {
-                    cursor,
-                    end_cursor,
-                    finality,
-                    batch,
-                } => {
-                    println!("data = {:?}", batch);
-                    // TODO: handle backoff
-                    sink.handle_data().await.unwrap();
+        loop {
+            tokio::select! {
+                _ = ct.cancelled() => {
+                    return Ok(())
                 }
-                DataMessage::Invalidate { cursor } => {
-                    println!("invalidate = {:?}", cursor);
-                    // TODO: handle backoff
-                    sink.handle_invalidate().await.unwrap();
+                maybe_message = data_stream.try_next() => {
+                    match maybe_message.map_err(SinkConnectorError::Stream)? {
+                        None => {
+                            warn!("data stream closed");
+                            return Ok(())
+                        }
+                        Some(message) => {
+                            self.handle_message(message, &mut sink, ct.clone()).await?;
+                        }
+                    }
                 }
             }
         }
-        Ok(())
+    }
+
+    async fn handle_message<S>(
+        &self,
+        message: DataMessage<B>,
+        sink: &mut S,
+        _ct: CancellationToken,
+    ) -> Result<(), SinkConnectorError>
+    where
+        S: Sink<B>,
+    {
+        match message {
+            DataMessage::Data {
+                cursor,
+                end_cursor,
+                finality,
+                batch,
+            } => {
+                debug!(cursor = ?cursor, end_cursor = ?end_cursor, "received data");
+                // TODO: handle backoff
+                sink.handle_data(cursor, end_cursor, finality, batch)
+                    .await
+                    .unwrap();
+                Ok(())
+            }
+            DataMessage::Invalidate { cursor } => {
+                debug!(cursor = ?cursor, "received invalidate");
+                // TODO: handle backoff
+                sink.handle_invalidate(cursor).await.unwrap();
+                Ok(())
+            }
+        }
     }
 }
