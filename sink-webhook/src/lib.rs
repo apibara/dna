a56664619362dc26560fae74ea3abcb919342bbc
Francesco Ceccon <francesco@ceccon.me>
2023-04-27 09:28:46 +0100

    webhook: call user-provided sink for each message

--- sink-webhook/src/lib.rs
+++ sink-webhook/src/lib.rs
@@ -1,3 +1,7 @@
+use apibara_core::{
+    node::v1alpha2::{Cursor, DataFinality},
+    starknet::v1alpha2::Block,
+};
 use apibara_sink_common::Sink;
 use async_trait::async_trait;
 
@@ -7,14 +11,22 @@ pub enum WebhookError {}
 pub struct WebhookSink {}
 
 #[async_trait]
-impl Sink for WebhookSink {
+impl Sink<Block> for WebhookSink {
     type Error = WebhookError;
 
-    async fn handle_data(&mut self) -> Result<(), Self::Error> {
+    async fn handle_data(
+        &mut self,
+        cursor: Option<Cursor>,
+        end_cursor: Cursor,
+        _finality: DataFinality,
+        _batch: Vec<Block>,
+    ) -> Result<(), Self::Error> {
+        println!("calling web hook with data {:?} - {:?}", cursor, end_cursor);
         Ok(())
     }
 
-    async fn handle_invalidate(&mut self) -> Result<(), Self::Error> {
+    async fn handle_invalidate(&mut self, cursor: Option<Cursor>) -> Result<(), Self::Error> {
+        println!("calling web hook with invalidate {:?}", cursor);
         Ok(())
     }
 }
