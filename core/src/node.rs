899cf2eb981b9f968366b5ef16fdd3195df88cab
Francesco Ceccon <francesco@ceccon.me>
2023-04-27 20:34:41 +0100

    webhook: post json data to target url

--- core/src/node.rs
+++ core/src/node.rs
@@ -1,4 +1,6 @@
 pub mod v1alpha2 {
+    use serde::ser::{Serialize, SerializeStruct, Serializer};
+
     tonic::include_proto!("apibara.node.v1alpha2");
 
     pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
@@ -6,5 +8,60 @@ pub mod v1alpha2 {
 
     pub fn node_file_descriptor_set() -> &'static [u8] {
         FILE_DESCRIPTOR_SET
+    }
+
+    impl Serialize for Cursor {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let mut state = serializer.serialize_struct("Cursor", 2)?;
+            state.serialize_field("order_key", &self.order_key)?;
+            let hex_key = format!("0x{}", hex::encode(&self.unique_key));
+            state.serialize_field("unique_key", &hex_key)?;
+            state.end()
+        }
+    }
+
+    impl Serialize for DataFinality {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let as_str = match self {
+                DataFinality::DataStatusFinalized => "finalized",
+                DataFinality::DataStatusAccepted => "accepted",
+                DataFinality::DataStatusPending => "pending",
+                DataFinality::DataStatusUnknown => "unknown",
+            };
+            serializer.serialize_str(as_str)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::node::v1alpha2::DataFinality;
+
+    #[test]
+    fn test_cursor_serialization() {
+        let cursor = super::v1alpha2::Cursor {
+            order_key: 1,
+            unique_key: vec![0, 1, 2, 3],
+        };
+        let serialized = serde_json::to_string(&cursor).unwrap();
+        assert_eq!(serialized, r#"{"order_key":1,"unique_key":"0x00010203"}"#);
+    }
+
+    #[test]
+    fn test_data_finality_serialization() {
+        let serialized = serde_json::to_string(&DataFinality::DataStatusUnknown).unwrap();
+        assert_eq!(serialized, r#""unknown""#);
+        let serialized = serde_json::to_string(&DataFinality::DataStatusPending).unwrap();
+        assert_eq!(serialized, r#""pending""#);
+        let serialized = serde_json::to_string(&DataFinality::DataStatusAccepted).unwrap();
+        assert_eq!(serialized, r#""accepted""#);
+        let serialized = serde_json::to_string(&DataFinality::DataStatusFinalized).unwrap();
+        assert_eq!(serialized, r#""finalized""#);
     }
 }
--- sink-webhook/src/lib.rs
+++ sink-webhook/src/lib.rs
@@ -1,17 +1,52 @@
+use std::str::FromStr;
+
 use apibara_core::{
     node::v1alpha2::{Cursor, DataFinality},
     starknet::v1alpha2::Block,
 };
 use apibara_sink_common::Sink;
 use async_trait::async_trait;
+use http::{uri::InvalidUri, Uri};
+use reqwest::Client;
+use serde::ser::Serialize;
+use serde_json::json;
+use tracing::{debug, info, warn};
 
 #[derive(Debug, thiserror::Error)]
 pub enum WebhookError {
-    #[error("test error")]
-    Test,
+    #[error("Http error: {0}")]
+    Http(#[from] reqwest::Error),
 }
 
-pub struct WebhookSink {}
+pub struct WebhookSink {
+    client: Client,
+    target_url: String,
+}
+
+impl WebhookSink {
+    pub fn new(target_url: String) -> Result<Self, InvalidUri> {
+        let _ = Uri::from_str(&target_url)?;
+        Ok(Self {
+            client: Client::new(),
+            target_url,
+        })
+    }
+
+    async fn send<B: Serialize + ?Sized>(&self, body: &B) -> Result<(), WebhookError> {
+        let response = self.client.post(&self.target_url).json(body).send().await?;
+
+        match response.text().await {
+            Ok(text) => {
+                debug!(response = ?text, "call success");
+            }
+            Err(err) => {
+                warn!(err = ?err, "error reading response");
+            }
+        }
+
+        Ok(())
+    }
+}
 
 #[async_trait]
 impl Sink<Block> for WebhookSink {
@@ -21,15 +56,30 @@ impl Sink<Block> for WebhookSink {
         &mut self,
         cursor: &Option<Cursor>,
         end_cursor: &Cursor,
-        _finality: &DataFinality,
-        _batch: &[Block],
+        finality: &DataFinality,
+        batch: &[Block],
     ) -> Result<(), Self::Error> {
-        println!("calling web hook with data {:?} - {:?}", cursor, end_cursor);
-        Ok(())
+        info!(cursor = ?cursor, end_cursor = ?end_cursor, "calling web hook with data");
+        let body = json!({
+            "data": {
+                "cursor": cursor,
+                "end_cursor": end_cursor,
+                "finality": finality,
+                "batch": batch,
+            },
+        });
+
+        self.send(&body).await
     }
 
     async fn handle_invalidate(&mut self, cursor: &Option<Cursor>) -> Result<(), Self::Error> {
-        println!("calling web hook with invalidate {:?}", cursor);
-        Ok(())
+        info!(cursor = ?cursor, "calling web hook with invalidate");
+        let body = json!({
+            "invalidate": {
+                "cursor": cursor,
+            },
+        });
+
+        self.send(&body).await
     }
 }
