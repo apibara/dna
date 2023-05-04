46e9623520252db759c4d3e6d955b69057dd1b66
Francesco Ceccon <francesco@ceccon.me>
2023-05-04 19:33:18 +0100

    starknet: refactor and export start node function

--- starknet/src/bin.rs
+++ starknet/src/bin.rs
@@ -1,15 +1,6 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
-use apibara_node::{db::default_data_dir, o11y::init_opentelemetry};
-use apibara_starknet::{
-    server::{MetadataKeyRequestObserver, SimpleRequestObserver},
-    HttpProvider, NoWriteMap, StarkNetNode,
-};
-use clap::{Args, Parser, Subcommand};
-use tempdir::TempDir;
-use tokio_util::sync::CancellationToken;
-use tracing::info;
+use apibara_starknet::{start_node, StartArgs};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -21,69 +12,12 @@ struct Cli {
 #[derive(Subcommand)]
 enum CliCommand {
     /// Start the StarkNet source node.
-    Start(StartCommand),
-}
-
-#[derive(Args)]
-struct StartCommand {
-    /// StarkNet RPC address.
-    #[arg(long, env)]
-    rpc: String,
-    /// Data directory. Defaults to `$XDG_DATA_HOME`.
-    #[arg(long, env)]
-    data: Option<PathBuf>,
-    /// Indexer name. Defaults to `starknet`.
-    #[arg(long, env)]
-    name: Option<String>,
-    /// Wait for RPC to be available before starting.
-    #[arg(long, env)]
-    wait_for_rpc: bool,
-    /// Create a temporary directory for data, deleted when devnet is closed.
-    #[arg(long, env)]
-    devnet: bool,
-    /// Use the specified metadata key for tracing and metering.
-    #[arg(long, env)]
-    use_metadata: Vec<String>,
-}
-
-async fn start(args: StartCommand) -> Result<()> {
-    init_opentelemetry()?;
-
-    let mut node =
-        StarkNetNode::<HttpProvider, SimpleRequestObserver, NoWriteMap>::builder(&args.rpc)?
-            .with_request_observer(MetadataKeyRequestObserver::new(args.use_metadata));
-
-    // Setup cancellation for graceful shutdown
-    let cts = CancellationToken::new();
-    ctrlc::set_handler({
-        let cts = cts.clone();
-        move || {
-            cts.cancel();
-        }
-    })?;
-
-    if args.devnet {
-        let tempdir = TempDir::new("apibara")?;
-        info!("starting in devnet mode");
-        node.with_datadir(tempdir.path().to_path_buf());
-    } else if let Some(datadir) = args.data {
-        info!("using user-provided datadir");
-        node.with_datadir(datadir);
-    } else if let Some(name) = args.name {
-        let datadir = default_data_dir()
-            .map(|p| p.join(name))
-            .expect("no datadir");
-        node.with_datadir(datadir);
-    }
-
-    node.build()?.start(cts.clone(), args.wait_for_rpc).await?;
-
-    Ok(())
+    Start(StartArgs),
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     match Cli::parse().command {
-        CliCommand::Start(args) => start(args).await,
+        CliCommand::Start(args) => start_node(args).await,
     }
 }
