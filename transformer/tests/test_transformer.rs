e7d8d726dc85157b171c408eda04866eaf0b2088
Francesco Ceccon <francesco@ceccon.me>
2023-05-31 15:30:34 +0100

    transformer: use deno_runtime

--- transformer/tests/test_transformer.rs
+++ transformer/tests/test_transformer.rs
@@ -5,27 +5,32 @@ use apibara_transformer::Transformer;
 use serde_json::json;
 use tempfile::NamedTempFile;
 
-fn write_source(code: &str) -> NamedTempFile {
-    let tempfile = tempfile::Builder::new().suffix(".js").tempfile().unwrap();
+fn write_source(ext: &str, code: &str) -> NamedTempFile {
+    let tempfile = tempfile::Builder::new()
+        .suffix(&format!(".{}", ext))
+        .tempfile()
+        .unwrap();
     fs::write(tempfile.path(), code).unwrap();
     tempfile
 }
 
-async fn new_transformer_with_code(code: &str) -> Transformer {
-    let file = write_source(code);
-    Transformer::from_file(
+async fn new_transformer_with_code(ext: &str, code: &str) -> (NamedTempFile, Transformer) {
+    let file = write_source(ext, code);
+    let transformer = Transformer::from_file(
         file.path().to_str().unwrap(),
         std::env::current_dir().unwrap(),
     )
-    .await
-    .unwrap()
+    .unwrap();
+    (file, transformer)
 }
 
 #[tokio::test]
 async fn test_identity() {
-    let mut transformer = new_transformer_with_code(
+    let (_file, mut transformer) = new_transformer_with_code(
+        "js",
         r#"
         export default function (data) {
+            console.log('data', data);
             return data;
         }
         "#,
@@ -41,7 +46,8 @@ async fn test_identity() {
 
 #[tokio::test]
 async fn test_return_data_is_different() {
-    let mut transformer = new_transformer_with_code(
+    let (_file, mut transformer) = new_transformer_with_code(
+        "js",
         r#"
         export default function (data) {
             return data.map(({ foo }) => {
@@ -65,3 +71,60 @@ async fn test_return_data_is_different() {
     }]);
     assert_eq!(result, expected);
 }
+
+/*
+#[tokio::test]
+async fn test_import_library_over_http() {
+    let (_file, mut transformer) = new_transformer_with_code(
+        "js",
+        r#"
+        import capitalizeKeys from 'https://cdn.jsdelivr.net/gh/stdlib-js/utils-capitalize-keys@deno/mod.js';
+                                          //
+        export default function (data) {
+            return data.map((block) => {
+                return capitalizeKeys(block);
+            });
+        }
+        "#,
+    )
+    .await;
+    let input = json!([{
+        "foo": "bar",
+        "baz": 42,
+    }, {
+        "foo": "bux",
+    }]);
+    let result = transformer.transform(&input).await.unwrap();
+    let expected = json!([{
+        "Foo": "bar",
+        "Baz": 42,
+    }, {
+        "Foo": "bux",
+    }]);
+    assert_eq!(result, expected);
+}
+
+#[tokio::test]
+async fn test_typescript() {
+    let (_file, mut transformer) = new_transformer_with_code(
+        "ts",
+        r#"
+        interface Data {
+            foo: string;
+            bar: number;
+        }
+
+        export default function (data: Data): Data {
+            return data;
+        }
+        "#,
+    )
+    .await;
+    let input = json!({
+        "foo": "bar",
+        "baz": 42,
+    });
+    let result = transformer.transform(&input).await.unwrap();
+    assert_eq!(result, input);
+}
+*/
