bb844de560564660088b5149df8cbcd0ceb17dfc
Francesco Ceccon <francesco@ceccon.me>
2023-06-10 11:55:43 +0100

    cross compile crates

--- nix/native.nix
+++ nix/native.nix
@@ -1,5 +1,9 @@
-{ pkgs, crane, rustToolchain, workspaceDir }:
+{ pkgs, crane, workspaceDir, crates }:
 let
+  rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" ];
+  };
+
   buildLib = import ./build.nix { inherit pkgs crane rustToolchain workspaceDir; };
 
   workspaceFmt = buildLib.cargoFmt (buildLib.commonArgs // {
@@ -20,50 +24,14 @@ let
     version = "0.0.0";
   });
 
-  apibara = {
-    sdk = buildLib.buildCrate { crate = ../sdk; };
-    starknet = buildLib.buildCrate { crate = ../starknet; };
-    sink-webhook = buildLib.buildCrate { crate = ../sink-webhook; };
-    sink-mongo = buildLib.buildCrate { crate = ../sink-mongo; };
-    sink-postgres = buildLib.buildCrate { crate = ../sink-postgres; };
-    sink-parquet = buildLib.buildCrate { crate = ../sink-parquet; };
-  };
+  built = buildLib.buildCrates crates;
 in
 {
   checks = {
     inherit workspaceFmt workspaceClippy;
   };
 
-  packages = {
+  packages = (built.packages // {
     tests = workspaceTest;
-    sdk = apibara.sdk.bin;
-    starknet = apibara.starknet.bin;
-    sink-webhook = apibara.sink-webhook.bin;
-    sink-mongo = apibara.sink-mongo.bin;
-    sink-postgres = apibara.sink-postgres.bin;
-    sink-parquet = apibara.sink-parquet.bin;
-
-    starknet-image = buildLib.dockerizeCrateBin {
-      crate = apibara.starknet;
-      ports = {
-        "7171/tcp" = { };
-      };
-    };
-
-    sink-webhook-image = buildLib.dockerizeCrateBin {
-      crate = apibara.sink-webhook;
-    };
-
-    sink-mongo-image = buildLib.dockerizeCrateBin {
-      crate = apibara.sink-mongo;
-    };
-
-    sink-postgres-image = buildLib.dockerizeCrateBin {
-      crate = apibara.sink-postgres;
-    };
-
-    sink-parquet-image = buildLib.dockerizeCrateBin {
-      crate = apibara.sink-parquet;
-    };
-  };
+  });
 }
