8104f34d2693457fb34570ef36be9c60d31ab70e
Big Herc <bigherc18@gmail.com>
2023-08-14 16:21:05 +0200

    webhook: improve tests

--- sink-webhook/tests/test_sink.rs
+++ sink-webhook/tests/test_sink.rs
@@ -4,12 +4,20 @@ use apibara_sink_webhook::{SinkWebhookConfiguration, SinkWebhookError, WebhookSi
 use http::HeaderMap;
 use serde_json::{json, Value};
 
-fn new_batch(size: usize) -> Value {
+fn new_batch(start_cursor: &Option<Cursor>, end_cursor: &Cursor) -> Value {
     let mut batch = Vec::new();
-    for i in 0..size {
+
+    let start_block_num = match start_cursor {
+        Some(cursor) => cursor.order_key,
+        None => 0,
+    };
+
+    let end_block_num = end_cursor.order_key;
+
+    for i in start_block_num..end_block_num {
         batch.push(json!({
-            "batchNum": i,
-            "batchStr": format!("batch_{}", i),
+            "block_num": i,
+            "block_str": format!("block_{}", i),
         }));
     }
     json!(batch)
@@ -34,17 +42,20 @@ async fn test_handle_data() -> Result<(), SinkWebhookError> {
 
     let mut sink = WebhookSink::new(config);
 
-    for i in 0..5 {
-        let cursor = Some(new_cursor(i));
-        let end_cursor = new_cursor(i + 1);
+    let batch_size = 2;
+    let num_batches = 5;
+
+    for order_key in 0..num_batches {
+        let cursor = Some(new_cursor(order_key * batch_size));
+        let end_cursor = new_cursor((order_key + 1) * batch_size);
         let finality = DataFinality::DataStatusFinalized;
-        let batch = new_batch(5);
+        let batch = new_batch(&cursor, &end_cursor);
 
         sink.handle_data(&cursor, &end_cursor, &finality, &batch)
             .await?;
 
         let requests = server.received_requests().await.unwrap();
-        assert_eq!(requests.len(), i as usize + 1);
+        assert_eq!(requests.len() as u64, order_key + 1);
         assert_eq!(
             requests.last().unwrap().body_json::<Value>()?,
             json!({
@@ -79,7 +90,7 @@ async fn test_handle_invalidate() -> Result<(), SinkWebhookError> {
         sink.handle_invalidate(&cursor).await?;
 
         let requests = server.received_requests().await.unwrap();
-        assert_eq!(requests.len(), i as usize + 1);
+        assert_eq!(requests.len() as u64, i + 1);
         assert_eq!(
             requests.last().unwrap().body_json::<Value>()?,
             json!({
@@ -105,18 +116,21 @@ async fn test_handle_data_raw() -> Result<(), SinkWebhookError> {
 
     let mut sink = WebhookSink::new(config);
 
-    for i in 0..5 {
-        let cursor = new_cursor(i);
-        let end_cursor = new_cursor(i + 1);
-        let finality = DataFinality::DataStatusFinalized;
-        let batch = new_batch(5);
+    let batch_size = 2;
+    let num_batches = 5;
 
-        sink.handle_data(&Some(cursor), &end_cursor, &finality, &batch)
+    for order_key in 0..num_batches {
+        let cursor = Some(new_cursor(order_key * batch_size));
+        let end_cursor = new_cursor((order_key + 1) * batch_size);
+        let finality = DataFinality::DataStatusFinalized;
+        let batch = new_batch(&cursor, &end_cursor);
+
+        sink.handle_data(&cursor, &end_cursor, &finality, &batch)
             .await?;
 
         let requests = server.received_requests().await.unwrap();
-        assert_eq!(requests.len(), i as usize + 1);
-        assert_eq!(requests[0].body_json::<Value>()?, batch);
+        assert_eq!(requests.len() as u64, order_key + 1);
+        assert_eq!(requests.last().unwrap().body_json::<Value>()?, batch);
     }
 
     Ok(())
