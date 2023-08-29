04757b8856f6395f8464ba23b879194a19453628
Francesco Ceccon <francesco@ceccon.me>
2023-08-29 14:48:21 +0200

    runner: add common grpc interface

---
+++ runners/runner-common/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod runner {
+    pub mod v1 {
+        tonic::include_proto!("apibara.runner.v1");
+
+        const FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("runner_v1_descriptor");
+
+        pub fn runner_file_descriptor_set() -> &'static [u8] {
+            FILE_DESCRIPTOR_SET
+        }
+    }
+}
