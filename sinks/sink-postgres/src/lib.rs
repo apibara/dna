694df9e2bf13e1c0a6d65206210ba616a98699f5
Francesco Ceccon <francesco@ceccon.me>
2023-11-06 12:42:03 +0100

    sink-postgres: add conditions to invalidate query
    **Summary**
    This commit adds an option to conditionally invalidate rows.
    This is useful when multiple indexers write to the same table.

--- sinks/sink-postgres/src/lib.rs
+++ sinks/sink-postgres/src/lib.rs
@@ -1,5 +1,5 @@
 mod configuration;
 mod sink;
 
-pub use self::configuration::{SinkPostgresConfiguration, SinkPostgresOptions};
+pub use self::configuration::{InvalidateColumn, SinkPostgresConfiguration, SinkPostgresOptions};
 pub use self::sink::{PostgresSink, SinkPostgresError};
