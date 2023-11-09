7fc49421e2a10e2e65bafc3c984e8fe27e18b570
Francesco Ceccon <francesco@ceccon.me>
2023-11-09 10:10:18 +0100

    downgrade glibc version used
    **Summary**
    Ubuntu LTS are using glibc 2.35, this commit pins an older version of nixpkgs
    that uses glibc 2.35.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Apibara development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
