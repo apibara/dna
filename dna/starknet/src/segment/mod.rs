87acf045db53988ebee3a674026dbc781883f100
Francesco Ceccon <francesco@ceccon.me>
2024-01-20 21:19:12 +0100

    dna: add command to inspect segments

--- dna/starknet/src/segment/mod.rs
+++ dna/starknet/src/segment/mod.rs
@@ -1,11 +1,12 @@
 mod builder;
 mod common;
+mod display;
 mod event;
 mod group_builder;
 mod header;
 mod snapshot;
 #[allow(dead_code, unused_imports, clippy::all)]
-mod store;
+pub mod store;
 
 pub use self::builder::{SegmentBuilder, SegmentEvent};
 pub use self::group_builder::{SegmentGroupBuilder, SegmentGroupEvent};
