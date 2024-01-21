38a3cae65f8c7efe07fe89eeb032f567742105f4
Francesco Ceccon <francesco@ceccon.me>
2024-01-21 16:44:38 +0100

    dna: use bitmap to iterate over segments/blocks

--- dna/starknet/src/segment/mod.rs
+++ dna/starknet/src/segment/mod.rs
@@ -4,10 +4,12 @@ mod display;
 mod event;
 mod group_builder;
 mod header;
+mod index;
 mod snapshot;
 #[allow(dead_code, unused_imports, clippy::all)]
 pub mod store;
 
 pub use self::builder::{SegmentBuilder, SegmentEvent};
+pub use self::display::VectorExt;
 pub use self::group_builder::{SegmentGroupBuilder, SegmentGroupEvent};
 pub use self::snapshot::Snapshot;
