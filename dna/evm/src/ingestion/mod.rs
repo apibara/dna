72b73fbbbe2d9e4af134a1cd07baa5e469dba765
Francesco Ceccon <francesco@ceccon.me>
2024-01-29 18:19:25 +0100

    dna: reduce number of rpc calls

--- dna/evm/src/ingestion/mod.rs
+++ dna/evm/src/ingestion/mod.rs
@@ -2,6 +2,6 @@ mod finalized;
 mod ingestor;
 mod provider;
 
-pub use self::finalized::{FinalizedBlockIngestor, IngestionEvent};
+pub use self::finalized::{FinalizedBlockIngestor, IngestionEvent, IngestorOptions};
 pub use self::ingestor::Ingestor;
 pub use self::provider::{models, RpcProvider, RpcProviderService};
