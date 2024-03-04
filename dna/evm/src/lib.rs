e578d7da5b02470229b6ee456221b979031773e2
Francesco Ceccon <francesco@ceccon.me>
2024-03-04 14:57:02 +0100

    dna: stub dna server

--- dna/evm/src/lib.rs
+++ dna/evm/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod core;
 pub mod ingestion;
 pub mod segment;
+pub mod server;
--- dna/protocol/src/lib.rs
+++ dna/protocol/src/lib.rs
@@ -1,6 +1,5 @@
-pub mod dna {
-    tonic::include_proto!("apibara.dna.v2");
-}
+pub mod dna;
+pub mod ingestion;
 
 pub mod evm {
     tonic::include_proto!("apibara.evm.v2");
