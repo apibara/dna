9a5f55b887ca8594987bb30039d89a438d091d36
Francesco Ceccon <francesco@ceccon.me>
2024-03-04 23:15:46 +0100

    dna: stream evm block headers

--- examples/console/evm_to_console.js
+++ examples/console/evm_to_console.js
@@ -1,19 +1,23 @@
 export const config = {
   streamUrl: "http://localhost:7070",
-  startingBlock: 0,
+  startingBlock: 3_000_000,
   network: "evm",
   filter: {
+    header: { weak: false },
     logs: [
       {
         address: "0x25c4a76E7d118705e7Ea2e9b7d8C59930d8aCD3b",
       },
     ],
   },
-  sinkType: "console",
-  sinkOptions: {},
+  sinkType: "mongo",
+  sinkOptions: {
+    database: "test",
+    collectionName: "headers",
+  },
 };
 
 // Transform each block using the function defined in starknet.js.
-export default function transform(block) {
-  return block;
+export default function transform({ header }) {
+  return header;
 }
