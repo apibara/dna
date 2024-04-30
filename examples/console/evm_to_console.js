6328525efdf607dec2adb9c0f88b951511f7b5d5
Francesco Ceccon <francesco@ceccon.me>
2024-04-30 16:22:48 +0200

    sink: improve deserialization of log topic

--- examples/console/evm_to_console.js
+++ examples/console/evm_to_console.js
@@ -15,12 +15,9 @@ export const config = {
       {
         strict: false,
         topics: [
-          {
-            value:
-              "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-          },
-          {},
-          {},
+          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+          null,
+          null,
         ],
       },
     ],
