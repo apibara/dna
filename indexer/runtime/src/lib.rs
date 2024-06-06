2e0102a3913eab3d1d7a5dbe955c5266ab2b12c1
Francesco Ceccon <francesco@ceccon.me>
2024-06-06 15:03:23 +0200

    cleanup repo and projects
    The main indexer experience in v2 will be based on the Typescript SDK.
    This commit removes all code related to indexers and sinks, leaving
    only the DNA server components.

--- indexer/runtime/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod ext;
-mod module_loader;
-mod runtime;
-
-pub use self::runtime::{DenoRuntime, DenoRuntimeError, DenoRuntimeOptions};
--- sinks/sink-webhook/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod configuration;
-mod sink;
-
-pub use self::configuration::{BodyMode, SinkWebhookConfiguration, SinkWebhookOptions};
-pub use self::sink::WebhookSink;
