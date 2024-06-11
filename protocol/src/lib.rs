19b7a60ad4cfe7df0eb8eb98c3d61558952862a1
Francesco Ceccon <francesco@ceccon.me>
2024-06-11 11:29:44 +0200

    protocol: refactor conversion helpers

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod client;
 pub mod dna;
+pub mod error;
 pub mod evm;
+mod helpers;
 pub mod starknet;
--- protocol/src/starknet.rs
+++ protocol/src/starknet.rs
@@ -1,3 +1,7 @@
 use error_stack::{report, Result, ResultExt};
 
+use crate::helpers::{impl_scalar_helpers, impl_scalar_traits, impl_u256_scalar};
+
 tonic::include_proto!("starknet.v2");
+
+impl_u256_scalar!(FieldElement);
