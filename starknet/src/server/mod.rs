e1096f9377fe352f5ce4891b96213ae0e73a3e9e
Francesco Ceccon <francesco@ceccon.me>
2024-06-11 19:40:31 +0200

    starknet: implement basic dna server

--- starknet/src/server/mod.rs
+++ starknet/src/server/mod.rs
@@ -4,41 +4,40 @@ use apibara_dna_common::{
     error::{DnaError, Result},
     ingestion::SnapshotChange,
     server::CursorProducerService,
-    storage::{LocalStorageBackend, StorageBackend},
+    storage::{AppStorageBackend, CachedStorage, LocalStorageBackend, StorageBackend},
 };
 use apibara_dna_protocol::dna::stream::dna_stream_file_descriptor_set;
 use error_stack::ResultExt;
 use futures_util::Stream;
+use service::DnaService;
 use tokio_util::sync::CancellationToken;
 use tonic::transport::Server as TonicServer;
 use tracing::info;
 
-use crate::server::service::DnaService;
-
 mod filter;
 mod service;
 
-pub struct DnaServer<S, C>
+pub struct DnaServer<C>
 where
-    S: StorageBackend + Send + Sync + 'static,
-    <S as StorageBackend>::Reader: Unpin + Send,
     C: Stream<Item = SnapshotChange> + Unpin + Send + Sync + 'static,
 {
-    /// Storage (possibly cached) for segmented data.
-    storage: S,
+    /// Cached storage for segmented data.
+    storage: CachedStorage<AppStorageBackend>,
     /// Local storage for temporary blocks.
     local_storage: LocalStorageBackend,
     /// Stream producing changes to the snapshot state.
     snapshot_changes: C,
 }
 
-impl<S, C> DnaServer<S, C>
+impl<C> DnaServer<C>
 where
-    S: StorageBackend + Send + Sync + 'static + Clone,
-    <S as StorageBackend>::Reader: Unpin + Send,
     C: Stream<Item = SnapshotChange> + Unpin + Send + Sync + 'static,
 {
-    pub fn new(storage: S, local_storage: LocalStorageBackend, snapshot_changes: C) -> Self {
+    pub fn new(
+        storage: CachedStorage<AppStorageBackend>,
+        local_storage: LocalStorageBackend,
+        snapshot_changes: C,
+    ) -> Self {
         Self {
             storage,
             local_storage,
@@ -69,13 +68,9 @@ where
                 }
             });
 
-        tokio::select! {
-            Err(err) = server_task => {
-                Err(err).change_context(DnaError::Fatal).attach_printable("ingestion gRPC server error")
-            }
-            else => {
-                Ok(())
-            }
-        }
+        server_task
+            .await
+            .change_context(DnaError::Fatal)
+            .attach_printable("ingestion gRPC server error")
     }
 }
