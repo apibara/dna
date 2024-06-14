5973e399831f006ec9fe7cf59b9e401820432b2d
Francesco Ceccon <francesco@ceccon.me>
2024-06-14 16:27:27 +0200

    starknet: send headers to client

--- starknet/src/segment/reader.rs
+++ starknet/src/segment/reader.rs
@@ -1,87 +1,80 @@
+use std::marker::PhantomData;
+
 use apibara_dna_common::{
     error::{DnaError, Result},
     segment::SegmentOptions,
-    storage::{CachedStorage, LocalStorageBackend, StorageBackend},
+    storage::{CachedStorage, StorageBackend},
 };
 use error_stack::ResultExt;
+use memmap2::Mmap;
+use rkyv::{de::deserializers::SharedDeserializeMap, validation::validators::DefaultValidator};
+use tracing::debug;
 
-use super::store;
-
-#[derive(Debug, Copy, Clone)]
-pub struct SegmentReaderOptions {
-    pub needs_receipts: bool,
-    pub needs_transactions: bool,
-    pub needs_events: bool,
-}
-
-pub struct SegmentReader<S: StorageBackend + Send> {
+pub struct LazySegment<S: StorageBackend + Send, T> {
     storage: CachedStorage<S>,
-    local_storage: LocalStorageBackend,
+    filename: String,
     segment_options: SegmentOptions,
-    options: SegmentReaderOptions,
+    current_segment_start: Option<u64>,
+    mmap: Option<Mmap>,
+    data: PhantomData<T>,
 }
 
-impl<S> SegmentReader<S>
+impl<S, T> LazySegment<S, T>
 where
     S: StorageBackend + Send,
     S::Reader: Unpin + Send,
+    T: rkyv::Archive,
 {
     pub fn new(
         storage: CachedStorage<S>,
-        local_storage: LocalStorageBackend,
         segment_options: SegmentOptions,
-        options: SegmentReaderOptions,
+        filename: impl Into<String>,
     ) -> Self {
         Self {
             storage,
-            local_storage,
+            filename: filename.into(),
             segment_options,
-            options,
+            current_segment_start: None,
+            mmap: None,
+            data: Default::default(),
         }
     }
 
-    pub async fn segment_group(&mut self, block_number: u64) -> Result<store::SegmentGroup> {
-        let segment_group_name = self.segment_options.format_segment_group_name(block_number);
+    pub async fn read<'a>(&'a mut self, block_number: u64) -> Result<T>
+    where
+        <T as rkyv::Archive>::Archived:
+            rkyv::Deserialize<T, SharedDeserializeMap> + rkyv::CheckBytes<DefaultValidator<'a>>,
+    {
+        // Read segment if any of these two conditions are met:
+        // - The segment is not loaded yet.
+        // - The segment is loaded but the block number is different.
+        let segment_start = self.segment_options.segment_start(block_number);
 
-        let bytes = self.storage.mmap("group", segment_group_name).await?;
+        let bytes = match (self.mmap.take(), self.current_segment_start) {
+            (Some(mmap), Some(current_segment_start)) if current_segment_start == segment_start => {
+                mmap
+            }
+            (Some(_), _) | (None, _) => {
+                let segment_name = self.segment_options.format_segment_name(segment_start);
+                debug!(filename = self.filename, segment_name, "mmap segment");
+                let mmap = self
+                    .storage
+                    .mmap(format!("segment/{segment_name}"), &self.filename)
+                    .await?;
+                mmap
+            }
+        };
 
-        let segment_group = rkyv::from_bytes::<store::SegmentGroup>(&bytes)
+        self.mmap = Some(bytes);
+        self.current_segment_start = Some(segment_start);
+        let bytes = self.mmap.as_ref().expect("mmapped bytes");
+
+        let segment = rkyv::from_bytes::<'_, T>(&bytes)
             .map_err(|_| DnaError::Io)
-            .attach_printable("failed to deserialize segment group")?;
-
-        Ok(segment_group)
-    }
-
-    pub async fn header(&mut self, block_number: u64) -> Result<store::BlockHeaderSegment> {
-        let segment_name = self.segment_options.format_segment_name(block_number);
-        let bytes = self
-            .storage
-            .mmap(format!("segment/{segment_name}"), "header")
-            .await?;
-        let segment = rkyv::from_bytes::<store::BlockHeaderSegment>(&bytes)
-            .map_err(|_| DnaError::Io)
-            .attach_printable("failed to deserialize block header")?;
+            .attach_printable_lazy(|| {
+                format!("failed to deserialize segment: {}", self.filename)
+            })?;
 
         Ok(segment)
-    }
-}
-
-impl SegmentReaderOptions {
-    pub fn merge(&self, other: &Self) -> Self {
-        Self {
-            needs_receipts: self.needs_receipts || other.needs_receipts,
-            needs_transactions: self.needs_transactions || other.needs_transactions,
-            needs_events: self.needs_events || other.needs_events,
-        }
-    }
-}
-
-impl Default for SegmentReaderOptions {
-    fn default() -> Self {
-        Self {
-            needs_receipts: false,
-            needs_transactions: false,
-            needs_events: false,
-        }
     }
 }
