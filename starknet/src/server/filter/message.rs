08fa4a96d42b50eac20b3bf3147fc2efcf3176e9
Francesco Ceccon <francesco@ceccon.me>
2024-06-16 22:57:24 +0200

    starknet: serialize all transaction and receipt types

--- starknet/src/server/filter/message.rs
+++ starknet/src/server/filter/message.rs
@@ -3,6 +3,7 @@ use apibara_dna_protocol::starknet;
 use crate::segment::store;
 
 pub struct MessageToL1Filter {
+    pub from_address: Option<store::FieldElement>,
     pub to_address: Option<store::FieldElement>,
     pub include_reverted: bool,
     pub include_transaction: bool,
@@ -16,19 +17,29 @@ impl MessageToL1Filter {
             return false;
         }
 
-        let Some(to_address) = &self.to_address else {
-            return true;
+        let from_address_match = if let Some(from_address) = &self.from_address {
+            from_address == &message.from_address
+        } else {
+            true
         };
 
-        to_address == &message.to_address
+        let to_address_match = if let Some(to_address) = &self.to_address {
+            to_address == &message.to_address
+        } else {
+            true
+        };
+
+        from_address_match && to_address_match
     }
 }
 
 impl From<starknet::MessageToL1Filter> for MessageToL1Filter {
     fn from(value: starknet::MessageToL1Filter) -> Self {
+        let from_address = value.from_address.map(store::FieldElement::from);
         let to_address = value.to_address.map(store::FieldElement::from);
 
         Self {
+            from_address,
             to_address,
             include_reverted: value.include_reverted.unwrap_or(false),
             include_transaction: value.include_transaction.unwrap_or(false),
