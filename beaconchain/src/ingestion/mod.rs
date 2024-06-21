5915e0a9f9e1865c1009bc79b1406286651b78f1
Francesco Ceccon <francesco@ceccon.me>
2024-06-21 19:22:06 +0200

    beaconchain: ingest single blocks worth of data

--- beaconchain/src/ingestion/mod.rs
+++ beaconchain/src/ingestion/mod.rs
@@ -5,5 +5,5 @@ mod single_block;
 
 pub use self::chain_tracker::ChainTracker;
 pub use self::ext::*;
-pub use self::provider::{BeaconApiError, BeaconApiProvider, BlockId};
+pub use self::provider::{models, BeaconApiError, BeaconApiProvider, BlockId};
 pub use self::single_block::BeaconChainBlockIngestion;
--- beaconchain/src/segment/store.rs
+++ beaconchain/src/segment/store.rs
@@ -1,6 +1,26 @@
-use std::collections::BTreeMap;
+use rkyv::{Archive, Deserialize, Serialize};
 
-use rkyv::{with::AsVec, Archive, Deserialize, Serialize};
+use crate::ingestion::models;
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct Address(pub [u8; 20]);
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct B256(pub [u8; 32]);
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct U256(pub [u8; 32]);
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct B384(pub [u8; 48]);
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct Bytes(pub Vec<u8>);
 
 /// A beacon chain slot.
 #[derive(Archive, Serialize, Deserialize, Debug)]
@@ -14,27 +34,92 @@ pub enum Slot<T> {
 #[archive(check_bytes)]
 pub struct BlockHeader {
     pub slot: u64,
-    // TODO: fill them in with the real fields.
-    pub proposer_index: bool,
-    pub parent_root: bool,
-    pub state_root: bool,
+    pub proposer_index: u32,
+    pub parent_root: B256,
+    pub state_root: B256,
+    pub randao_reveal: Bytes,
+    pub deposit_count: u64,
+    pub deposit_root: B256,
+    pub block_hash: B256,
+    pub graffiti: B256,
+    pub execution_payload: ExecutionPayload,
+    pub blob_kzg_commitments: Vec<B384>,
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct ExecutionPayload {
+    pub parent_hash: B256,
+    pub fee_recipient: Address,
+    pub state_root: B256,
+    pub receipts_root: B256,
+    pub logs_bloom: Bytes,
+    pub prev_randao: B256,
+    pub block_number: u64,
+    pub timestamp: u64,
 }
 
 #[derive(Archive, Serialize, Deserialize, Debug)]
 #[archive(check_bytes)]
 pub struct Transaction {
+    pub transaction_type: u64,
     pub transaction_index: u32,
+    pub transaction_hash: B256,
+    pub nonce: u64,
+    pub from: Address,
+    pub to: Option<Address>,
+    pub value: U256,
+    pub gas_price: Option<u128>,
+    pub gas_limit: u128,
+    pub max_fee_per_gas: Option<u128>,
+    pub max_priority_fee_per_gas: Option<u128>,
+    pub max_fee_per_blob_gas: Option<u128>,
+    pub input: Bytes,
+    pub signature: Signature,
+    pub chain_id: Option<u64>,
+    pub access_list: Vec<AccessListItem>,
+    pub blob_versioned_hashes: Vec<B256>,
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct Signature {
+    pub r: U256,
+    pub s: U256,
+}
+
+#[derive(Archive, Serialize, Deserialize, Debug)]
+#[archive(check_bytes)]
+pub struct AccessListItem {
+    pub address: Address,
+    pub storage_keys: Vec<B256>,
 }
 
 #[derive(Archive, Serialize, Deserialize, Debug)]
 #[archive(check_bytes)]
 pub struct Validator {
     pub validator_index: u32,
+    pub balance: u64,
+    pub status: models::ValidatorStatus,
+    pub pubkey: B384,
+    pub withdrawal_credentials: B256,
+    pub effective_balance: u64,
+    pub slashed: bool,
+    pub activation_eligibility_epoch: u64,
+    pub activation_epoch: u64,
+    pub exit_epoch: u64,
+    pub withdrawable_epoch: u64,
 }
 
 #[derive(Archive, Serialize, Deserialize, Debug)]
 #[archive(check_bytes)]
-pub struct Blob {}
+pub struct Blob {
+    pub index: u32,
+    pub blob: Bytes,
+    pub kzg_commitment: B384,
+    pub kzg_proof: B384,
+    pub kzg_commitment_inclusion_proof: Vec<B256>,
+}
 
 #[derive(Archive, Serialize, Deserialize, Debug)]
 #[archive(check_bytes)]
