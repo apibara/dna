1257c06a295e5ded3bffdd1c9cef7e24cf600f45
Francesco Ceccon <francesco@ceccon.me>
2024-06-26 12:23:23 +0200

    starknet: refactor `DnaServer` out of crate

--- common/src/server/mod.rs
+++ common/src/server/mod.rs
@@ -1,7 +1,9 @@
 mod cursor_producer;
+mod dna_server;
 mod state_sync;
 
 pub use self::cursor_producer::{
     BlockNumberOrCursor, CursorProducer, CursorProducerService, NextBlock,
 };
+pub use self::dna_server::{DnaServer, DnaServerError};
 pub use self::state_sync::IngestionStateSyncServer;
