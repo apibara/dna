fdec9c83935d5ec4717d3f736e92db3618742e9b
Francesco Ceccon <francesco@ceccon.me>
2024-06-26 10:53:16 +0200

    protocol: refactor scalar types

--- protocol/src/evm.rs
+++ protocol/src/evm.rs
@@ -1,61 +1,60 @@
 use error_stack::{report, Result, ResultExt};
 
-use crate::helpers::{impl_scalar_helpers, impl_scalar_traits, impl_u256_scalar};
+use crate::helpers::{
+    from_be_bytes_slice, impl_from_to_bytes, impl_scalar_helpers, impl_scalar_traits,
+};
 
 tonic::include_proto!("evm.v2");
 
 impl_scalar_traits!(Address);
+impl_from_to_bytes!(Address, 20);
 impl_scalar_helpers!(Address, 20);
 
-impl Address {
-    pub fn from_bytes(bytes: &[u8; 20]) -> Self {
-        let lo_lo = u64::from_be_bytes([
-            bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
-        ]);
-        let lo_hi = u64::from_be_bytes([
-            bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
-        ]);
-        let hi = u32::from_be_bytes([bytes[16], bytes[17], bytes[18], bytes[19]]);
+impl_scalar_traits!(U256);
+impl_from_to_bytes!(U256, 32);
+impl_scalar_helpers!(U256, 32);
 
-        Address { lo_lo, lo_hi, hi }
-    }
-
-    pub fn to_bytes(&self) -> [u8; 20] {
-        let lo_lo = self.lo_lo.to_be_bytes();
-        let lo_hi = self.lo_hi.to_be_bytes();
-        let hi = self.hi.to_be_bytes();
-        [
-            lo_lo[0], lo_lo[1], lo_lo[2], lo_lo[3], lo_lo[4], lo_lo[5], lo_lo[6], lo_lo[7], //
-            lo_hi[0], lo_hi[1], lo_hi[2], lo_hi[3], lo_hi[4], lo_hi[5], lo_hi[6], lo_hi[7], //
-            hi[0], hi[1], hi[2], hi[3],
-        ]
-    }
-}
+impl_scalar_traits!(B256);
+impl_from_to_bytes!(B256, 32);
+impl_scalar_helpers!(B256, 32);
 
 impl_scalar_traits!(U128);
+impl_from_to_bytes!(U128, 16);
 impl_scalar_helpers!(U128, 16);
 
-impl U128 {
-    pub fn from_bytes(bytes: &[u8; 16]) -> Self {
-        let lo = u64::from_be_bytes([
-            bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
-        ]);
-        let hi = u64::from_be_bytes([
-            bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
-        ]);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        U128 { lo, hi }
+    #[test]
+    pub fn test_address() {
+        let hex = "0x27504265a9bc4330e3fe82061a60cd8b6369b4dc";
+        let address = Address::from_hex(hex).unwrap();
+        let back = address.to_hex();
+        assert_eq!(hex, &back);
     }
 
-    pub fn to_bytes(&self) -> [u8; 16] {
-        let lo = self.lo.to_be_bytes();
-        let hi = self.hi.to_be_bytes();
-        [
-            lo[0], lo[1], lo[2], lo[3], lo[4], lo[5], lo[6], lo[7], //
-            hi[0], hi[1], hi[2], hi[3], hi[4], hi[5], hi[6], hi[7], //
-        ]
+    #[test]
+    pub fn test_u256() {
+        let hex = "0x9df92d765b5aa041fd4bbe8d5878eb89290efa78e444c1a603eecfae2ea05fa4";
+        let u256 = U256::from_hex(hex).unwrap();
+        let back = u256.to_hex();
+        assert_eq!(hex, &back);
+    }
+
+    #[test]
+    pub fn test_b256() {
+        let hex = "0x9df92d765b5aa041fd4bbe8d5878eb89290efa78e444c1a603eecfae2ea05fa4";
+        let b256 = B256::from_hex(hex).unwrap();
+        let back = b256.to_hex();
+        assert_eq!(hex, &back);
+    }
+
+    #[test]
+    pub fn test_u128() {
+        let hex = "0x090efa78e444c1a603eecfae2ea05fa4";
+        let u256 = U128::from_hex(hex).unwrap();
+        let back = u256.to_hex();
+        assert_eq!(hex, &back);
     }
 }
-
-impl_u256_scalar!(U256);
-impl_u256_scalar!(B256);
