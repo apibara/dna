0f85fbef5fcebb3b70349dd186720d11e38a4077
Francesco Ceccon <francesco@ceccon.me>
2024-07-02 12:01:57 +0200

    evm: refactor cursor provider

--- evm/src/ingestion/chain_tracker.rs
+++ evm/src/ingestion/chain_tracker.rs
@@ -1,11 +1,9 @@
-use std::{sync::Arc, time::Duration};
+use std::{pin::Pin, sync::Arc, time::Duration};
 
 use apibara_dna_common::{core::Cursor, ingestion::CursorProvider};
 use error_stack::{Result, ResultExt};
-use futures_util::TryFutureExt;
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::ReceiverStream;
-use tracing::warn;
+use futures_util::{Stream, StreamExt};
+use tracing::{trace, warn};
 
 use crate::ingestion::models::BlockIdExt;
 
@@ -23,6 +21,7 @@ pub struct EvmCursorProvider {
 #[derive(Debug, Clone, Copy)]
 pub struct EvmCursorProviderOptions {
     pub poll_interval: Duration,
+    pub request_timeout: Duration,
 }
 
 impl EvmCursorProvider {
@@ -38,6 +37,7 @@ impl Default for EvmCursorProviderOptions {
     fn default() -> Self {
         Self {
             poll_interval: Duration::from_secs(3),
+            request_timeout: Duration::from_secs(5),
         }
     }
 }
@@ -45,43 +45,27 @@ impl Default for EvmCursorProviderOptions {
 #[async_trait::async_trait]
 impl CursorProvider for EvmCursorProvider {
     type Error = JsonRpcProviderError;
-    type CursorStream = ReceiverStream<Cursor>;
+    type CursorStream = Pin<Box<dyn Stream<Item = Cursor> + Send>>;
 
     async fn subscribe_head(&self) -> Result<Self::CursorStream, Self::Error> {
-        let (tx, rx) = mpsc::channel(1024);
-
-        tokio::spawn(
-            poll_block_id(
-                self.provider.clone(),
-                models::BlockId::Number(models::BlockNumberOrTag::Latest),
-                self.options,
-                tx,
-            )
-            .inspect_err(|err| {
-                warn!(error = ?err, "head block poll task failed");
-            }),
-        );
-
-        Ok(ReceiverStream::new(rx))
+        let stream = poll_block_id_stream(
+            self.provider.clone(),
+            models::BlockId::Number(models::BlockNumberOrTag::Latest),
+            self.options,
+        )
+        .await?;
+        Ok(stream.boxed())
     }
 
     /// Subscribe to changes to the current finalized block.
     async fn subscribe_finalized(&self) -> Result<Self::CursorStream, Self::Error> {
-        let (tx, rx) = mpsc::channel(1024);
-
-        tokio::spawn(
-            poll_block_id(
-                self.provider.clone(),
-                models::BlockId::Number(models::BlockNumberOrTag::Finalized),
-                self.options,
-                tx,
-            )
-            .inspect_err(|err| {
-                warn!(error = ?err, "finalized block poll task failed");
-            }),
-        );
-
-        Ok(ReceiverStream::new(rx))
+        let stream = poll_block_id_stream(
+            self.provider.clone(),
+            models::BlockId::Number(models::BlockNumberOrTag::Finalized),
+            self.options,
+        )
+        .await?;
+        Ok(stream.boxed())
     }
 
     /// Returns the cursor of the parent of the provided cursor.
@@ -98,39 +82,50 @@ impl CursorProvider for EvmCursorProvider {
     }
 }
 
-async fn poll_block_id(
+async fn poll_block_id_stream(
     provider: Arc<JsonRpcProvider>,
     id: models::BlockId,
     options: EvmCursorProviderOptions,
-    tx: mpsc::Sender<Cursor>,
-) -> Result<(), JsonRpcProviderError> {
-    let mut previous = get_block_id(&provider, id).await?;
-
-    tx.send(previous.clone())
+) -> Result<impl Stream<Item = Cursor>, JsonRpcProviderError> {
+    let mut previous = get_block_id(&provider, id, options.request_timeout)
         .await
-        .change_context(JsonRpcProviderError)?;
+        .attach_printable("failed to get starting block id")?;
 
-    loop {
-        let cursor = get_block_id(&provider, id).await?;
+    let stream = async_stream::stream! {
+        yield previous.clone();
 
-        if cursor != previous {
-            tx.send(cursor.clone())
-                .await
-                .change_context(JsonRpcProviderError)?;
-            previous = cursor;
+        loop {
+            trace!(?id, "polling block id");
+            let cursor = match get_block_id(&provider, id, options.request_timeout).await {
+                Ok(cursor) => cursor,
+                Err(err) => {
+                    warn!(error = ?err, "failed to poll block id");
+                    tokio::time::sleep(10 * options.poll_interval).await;
+                    continue;
+                }
+            };
+
+            if cursor != previous {
+                yield cursor.clone();
+                previous = cursor;
+            }
+
+            tokio::time::sleep(options.poll_interval).await;
         }
+    };
 
-        tokio::time::sleep(options.poll_interval).await;
-    }
+    Ok(stream)
 }
 
 async fn get_block_id(
     provider: &JsonRpcProvider,
     id: models::BlockId,
+    timeout: Duration,
 ) -> Result<Cursor, JsonRpcProviderError> {
-    let cursor = provider
-        .get_block(id)
-        .await?
+    let cursor = tokio::time::timeout(timeout, provider.get_block(id))
+        .await
+        .change_context(JsonRpcProviderError)
+        .attach_printable("timeout sending RPC request")??
         .ok_or(JsonRpcProviderError)
         .attach_printable("missing block")?
         .cursor()
