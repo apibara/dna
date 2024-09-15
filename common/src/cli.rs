ddaf5f74762e0b9a2b0cf4f86ab8d33d8bf72ecb
Francesco Ceccon <francesco@ceccon.me>
2024-09-15 09:49:08 +0200

    common: rename s3.endpoint-url to s3.endpoint

--- common/src/cli.rs
+++ common/src/cli.rs
@@ -9,16 +9,16 @@ use crate::object_store::{ObjectStore, ObjectStoreOptions};
 pub struct ObjectStoreArgs {
     /// The S3 bucket to use.
     #[arg(long = "s3.bucket", env = "DNA_S3_BUCKET")]
-    pub bucket: String,
+    pub s3_bucket: String,
     /// Under which prefix to store the data.
     #[arg(long = "s3.prefix", env = "DNA_S3_PREFIX")]
     pub s3_prefix: Option<String>,
     /// The S3 endpoint URL.
-    #[arg(long = "s3.endpoint-url", env = "DNA_S3_ENDPOINT_URL")]
-    pub endpoint_url: Option<String>,
+    #[arg(long = "s3.endpoint", env = "DNA_S3_ENDPOINT")]
+    pub s3_endpoint: Option<String>,
     /// The S3 region.
     #[arg(long = "s3.region", env = "DNA_S3_REGION")]
-    pub region: Option<String>,
+    pub s3_region: Option<String>,
 }
 
 #[derive(Args, Clone, Debug)]
@@ -46,14 +46,14 @@ impl ObjectStoreArgs {
     pub async fn into_object_store_client(self) -> ObjectStore {
         let mut config = aws_config::from_env();
 
-        if let Some(region) = self.region.as_ref() {
+        if let Some(region) = self.s3_region.as_ref() {
             let region = Region::new(region.clone());
             let region = RegionProviderChain::default_provider().or_else(region.clone());
 
             config = config.region(region);
         }
 
-        if let Some(endpoint_url) = self.endpoint_url.as_ref() {
+        if let Some(endpoint_url) = self.s3_endpoint.as_ref() {
             config = config.endpoint_url(endpoint_url.clone());
         }
 
@@ -64,7 +64,7 @@ impl ObjectStoreArgs {
             .build();
 
         let options = ObjectStoreOptions {
-            bucket: self.bucket,
+            bucket: self.s3_bucket,
             prefix: self.s3_prefix,
         };
 
