3d33bbf6d0fc0a21931763e4cd92042334cd5bd1
Francesco Ceccon <francesco@ceccon.me>
2024-11-06 15:06:48 +0100

    ci: update nextest to use ci profile

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Apibara development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
