0a640bf9f4d86c4d05c0a0620288bb7fdb81cbf7
Francesco Ceccon <francesco@ceccon.me>
2025-07-01 16:23:11 +0200

    metadata: stub admin api

---
+++ metadata/core/build.rs
@@ -0,0 +1,16 @@
+use std::{env, io::Result, path::PathBuf, println};
+
+static ADMIN_DESCRIPTOR_FILE: &str = "wings_v1_admin.bin";
+
+fn main() -> Result<()> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    println!("cargo:rerun-if-changed=proto");
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join(ADMIN_DESCRIPTOR_FILE))
+        .compile_protos(&["proto/wings/admin.proto"], &["proto/"])?;
+
+    Ok(())
+}
